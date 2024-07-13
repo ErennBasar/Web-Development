dddf7ddb1f4870c0b348470ad455a923a1379b9e
Eren Başar <erenbasar16@hotmail.com>
2024-07-13 19:02:20 +0300

    music list ve ileri,geri metodları oluşturuldu

---
+++ javascript/Uygulamalar/MusicPlayer/musicplayer.js
@@ -0,0 +1,21 @@
+class MusicPlayer {
+  constructor(musicList) {
+    this.musicList = musicList;
+    this.index = 0;
+  }
+  getMusic() {
+    return this.musicList[this.index];
+  }
+  next() {
+    this.index++;
+    if (this.index >= this.musicList.length) {
+      this.index = 0;
+    }
+  }
+  previous() {
+    this.index--;
+    if (this.index < 0) {
+      this.index = this.musicList.length - 1;
+    }
+  }
+}
