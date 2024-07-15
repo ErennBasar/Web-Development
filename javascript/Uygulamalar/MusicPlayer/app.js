fa97858da4810afc7e842889962ea5526694cb47
Eren Başar <erenbasar16@hotmail.com>
2024-07-15 19:42:45 +0300

    move to selected music function has been created

--- javascript/Uygulamalar/MusicPlayer/app.js
+++ javascript/Uygulamalar/MusicPlayer/app.js
@@ -20,6 +20,7 @@ window.addEventListener("load", () => {
   let music = player.getMusic();
   displayMusic(music);
   displayMusicList(player.musicList);
+  isPlayingNow();
 });
 
 function displayMusic(music) {
@@ -47,6 +48,7 @@ const prevMusic = () => {
   let music = player.getMusic();
   displayMusic(music);
   playMusic();
+  isPlayingNow();
 };
 
 const nextMusic = () => {
@@ -54,6 +56,7 @@ const nextMusic = () => {
   let music = player.getMusic();
   displayMusic(music);
   playMusic();
+  isPlayingNow();
 };
 
 const pauseMusic = () => {
@@ -128,7 +131,7 @@ const displayMusicList = (list) => {
   for (let i = 0; i < list.length; i++) {
     let liTag = `
 
-           <li class ="list-group-item d-flex justify-content-between align-items-center ">
+           <li li-index= ${i} onclick="selectedMusic(this)" class ="list-group-item d-flex justify-content-between align-items-center ">
              <span> ${list[i].getName()} </span>
              <span id="music-${i}" class="badge bg-primary rounded-pill  "></span>
              <audio class="music-${i}" src="mp3/${list[i].file}"></audio> 
@@ -146,3 +149,28 @@ const displayMusicList = (list) => {
     });
   }
 };
+
+const selectedMusic = (li) => {
+  const index = li.getAttribute("li-index");
+  // console.log(index);  // tıklanan müziğin indexini console.log ile kontrol edebiliriz.
+  player.index = index;
+  player.getMusic();
+  displayMusic(player.getMusic());
+  playMusic();
+  isPlayingNow();
+};
+
+const isPlayingNow = () => {
+  for (let li of ul.querySelectorAll("li")) {
+    if (li.classList.contains("playing")) {
+      li.classList.remove("playing");
+    }
+    if (li.getAttribute("li-index") == player.index) {
+      li.classList.add("playing");
+    }
+  }
+};
+
+audio.addEventListener("ended", () => {
+  nextMusic();
+});
