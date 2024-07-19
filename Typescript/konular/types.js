520543dbf0614a9e216fd7a40967afb87661f8ad
Eren Basar <erenbasar16@hotmail.com>
2024-07-19 15:54:43 +0300

    typescript types examples

--- Typescript/konular/types.js
+++ Typescript/konular/types.js
@@ -1,5 +1,19 @@
-var number;
-number = 10;
-number = true;
-number = "a";
-console.log(number);
+var a = 5;
+var b = "a";
+var c = true;
+var d;
+var e = [1, 2, 3];
+var f = [1, 2, 3];
+var g = [1, "a", true];
+var h = ["a", 5, false]; //tuple
+var krediPayment = 0;
+var havalePayment = 1;
+var eftPayment = 2;
+var Payment;
+(function (Payment) {
+    Payment[Payment["kredi"] = 10] = "kredi";
+    Payment[Payment["havale"] = 11] = "havale";
+    Payment[Payment["eft"] = 12] = "eft";
+})(Payment || (Payment = {}));
+var payment = Payment.havale;
+console.log(payment);
--- Typescript/konular/types.ts
+++ Typescript/konular/types.ts
@@ -1,6 +1,21 @@
-let number;
-number = 10;
-number = true;
-number = "a";
+let a: number = 5;
+let b: string = "a";
+let c: boolean = true;
+let d: any;
+let e: number[] = [1, 2, 3];
+let f: Array<number> = [1, 2, 3];
+let g: any[] = [1, "a", true];
+let h: [string, number, boolean] = ["a", 5, false]; //tuple
 
-console.log(number);
+const krediPayment = 0;
+const havalePayment = 1;
+const eftPayment = 2;
+
+enum Payment {
+  kredi = 10,
+  havale,
+  eft,
+}
+
+let payment = Payment.havale;
+console.log(payment);
