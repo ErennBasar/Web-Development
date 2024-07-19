c664954bb60a02b92bcec1a1f444a84eb473d1ab
Eren Basar <erenbasar16@hotmail.com>
2024-07-19 23:45:48 +0300

    constructor example

---
+++ Typescript/konular/constructor.ts
@@ -0,0 +1,29 @@
+interface point {
+  x: number;
+  y: number;
+}
+
+interface vehicle {
+  currentLocation: point;
+  travelTo(point: point): void;
+}
+
+class Taxi implements vehicle {
+  color: string;
+
+  constructor(location: point, color?: string) {
+    this.currentLocation = location;
+  }
+  currentLocation: point;
+  travelTo(point: point): void {
+    console.log(`Taxi X: ${point.x} Y: ${point.y} konumuna gidiyor.`);
+  }
+}
+
+let taxi_1: Taxi = new Taxi({ x: 2, y: 5 });
+
+taxi_1.travelTo({ x: 1, y: 2 });
+//taxi_1.color = "Red";
+console.log(taxi_1.currentLocation);
+
+let taxi_2: Taxi = new Taxi({ x: 3, y: 6 }, "Red");
