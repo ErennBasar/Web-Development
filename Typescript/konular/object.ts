542cf0188554dda810b6670d4bd3bd2a62ef41a3
Eren Basar <erenbasar16@hotmail.com>
2024-07-19 23:34:14 +0300

    object example

---
+++ Typescript/konular/object.ts
@@ -0,0 +1,38 @@
+interface point {
+  x: number;
+  y: number;
+}
+
+interface vehicle {
+  currentLocation: point;
+  travelTo(point: point): void;
+}
+
+class Taxi implements vehicle {
+  currentLocation: point;
+  travelTo(point: point): void {
+    console.log(`Taxi X: ${point.x} Y: ${point.y} konumuna gidiyor.`);
+  }
+}
+
+class Bus implements vehicle {
+  currentLocation: point;
+  travelTo(point: point): void {
+    console.log(`Bus X: ${point.x} Y: ${point.y} konumuna gidiyor.`);
+  }
+}
+let taxi_1: Taxi = new Taxi();
+
+taxi_1.travelTo({ x: 1, y: 2 });
+
+taxi_1.currentLocation = { x: 2, y: 5 };
+
+let taxi_2: Taxi = new Taxi();
+taxi_2.travelTo({ x: 5, y: 7 });
+taxi_2.currentLocation = { x: 3, y: 6 };
+
+console.log(taxi_1.currentLocation.x);
+console.log(taxi_1.currentLocation.y);
+
+console.log(taxi_2.currentLocation.x);
+console.log(taxi_2.currentLocation.y);
