3cd4b208d9c446beb2aa87de94f29139d00ccba7
Eren Basar <erenbasar16@hotmail.com>
2024-07-20 14:41:38 +0300

    file imported

--- Typescript/exercise/IProductService.ts
+++ Typescript/exercise/IProductService.ts
@@ -1,7 +1,7 @@
 import { Product } from "./Product";
 
 export interface IProductService {
-  getById(id: Number): Product;
+  getById(id: number): Product;
   getProduct(): Array<Product>;
   saveProduct(product: Product): void;
   deleteProduct(product: Product): void;
