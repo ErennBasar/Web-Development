276726fe44dfd82dce84a15f42f32c918f8b44c4
Eren Basar <erenbasar16@hotmail.com>
2024-07-20 15:29:11 +0300

    updated

--- Typescript/exercise/IProductService.ts
+++ Typescript/exercise/IProductService.ts
@@ -2,7 +2,7 @@ import { Product } from "./Product";
 
 export interface IProductService {
   getById(id: number): Product;
-  getProduct(): Array<Product>;
+  getProducts(): Array<Product>;
   saveProduct(product: Product): void;
   deleteProduct(product: Product): void;
 }
