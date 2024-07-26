6f18db98ae381ab1a3e8125203fac54f2c386e72
Eren Basar <erenbasar16@hotmail.com>
2024-07-26 20:36:53 +0300

    product 1 has been added

--- ng-app/src/app/product-list/product-list.component.ts
+++ ng-app/src/app/product-list/product-list.component.ts
@@ -8,4 +8,14 @@ import { ProductComponent } from './product/product.component';
   templateUrl: './product-list.component.html',
   styleUrl: './product-list.component.css',
 })
-export class ProductListComponent {}
+export class ProductListComponent {
+  product = {
+    id: 1,
+    name: 'Iphone 14',
+    price: 40000,
+    isActive: true,
+    description: 'iyi telefon',
+    imageUrl:
+      'https://store.storeimages.cdn-apple.com/1/as-images.apple.com/is/iphone-14-model-unselect-gallery-2-202303_GEO_EMEA?wid=5120&hei=2880&fmt=webp&qlt=70&.v=NjB6M3BqTGRudDZtakJrUG5tT2pHTGdzSmpObkZCM3MrNmJ5SkhESlNDZ1hBSXMwL2Jwdk9oTk42KzZHdTdNUXovdld4NkVCZ3JUZXJyZ1dUb1MwM0dKTG1lVWJJT2RXQWE0Mm9rU1V0V0IxZG1zQmhZY3FpN094bFJrYnF5eDR2azA1RzdtcFQ5a1dBaURTY0hJUEJBPT0=&traceId=1',
+  };
+}
