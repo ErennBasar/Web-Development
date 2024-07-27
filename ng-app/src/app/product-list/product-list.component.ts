ef125ffbff58ad00ce05615a08ee94854beb8434
Eren Basar <erenbasar16@hotmail.com>
2024-07-27 16:17:45 +0300

    *ngIf statement has been added

--- ng-app/src/app/product-list/product-list.component.ts
+++ ng-app/src/app/product-list/product-list.component.ts
@@ -22,12 +22,12 @@ export class ProductListComponent {
 
   productList = ['Iphone 15', 'Iphone14', 'Iphone 13'];
 
-  products = [
+  products: any = [
     {
       id: 1,
       name: 'Iphone 15',
       price: 50000,
-      isActive: true,
+      isActive: false,
       description: 'iyi telefon',
       imageUrl:
         'https://store.storeimages.cdn-apple.com/1/as-images.apple.com/is/iphone-14-model-unselect-gallery-2-202303_GEO_EMEA?wid=5120&hei=2880&fmt=webp&qlt=70&.v=NjB6M3BqTGRudDZtakJrUG5tT2pHTGdzSmpObkZCM3MrNmJ5SkhESlNDZ1hBSXMwL2Jwdk9oTk42KzZHdTdNUXovdld4NkVCZ3JUZXJyZ1dUb1MwM0dKTG1lVWJJT2RXQWE0Mm9rU1V0V0IxZG1zQmhZY3FpN094bFJrYnF5eDR2azA1RzdtcFQ5a1dBaURTY0hJUEJBPT0=&traceId=1',
