987f12478fb112b13bd3ed066e873bed129acb11
Eren Basar <erenbasar16@hotmail.com>
2024-07-27 14:50:55 +0300

    CommonModule has been added for *ngFor

--- ng-app/src/app/app.component.ts
+++ ng-app/src/app/app.component.ts
@@ -2,11 +2,12 @@ import { Component } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
 import { NavbarComponent } from './navbar/navbar.component';
 import { ProductListComponent } from './product-list/product-list.component';
+import { CommonModule } from '@angular/common';
 
 @Component({
   selector: 'app-root',
   standalone: true,
-  imports: [RouterOutlet, NavbarComponent, ProductListComponent],
+  imports: [RouterOutlet, NavbarComponent, ProductListComponent, CommonModule],
   templateUrl: './app.component.html',
   styleUrl: './app.component.css',
 })
