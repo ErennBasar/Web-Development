dc4e8e0f026fa75acf15fe4b267d1eef4effefb8
Eren Basar <erenbasar16@hotmail.com>
2024-07-28 16:22:19 +0300

    updated

--- ng-app/src/app/app.component.ts
+++ ng-app/src/app/app.component.ts
@@ -3,11 +3,18 @@ import { RouterOutlet } from '@angular/router';
 import { NavbarComponent } from './navbar/navbar.component';
 import { ProductListComponent } from './product-list/product-list.component';
 import { CommonModule } from '@angular/common';
+import { CategoryListComponent } from './category-list/category-list.component';
 
 @Component({
   selector: 'app-root',
   standalone: true,
-  imports: [RouterOutlet, NavbarComponent, ProductListComponent, CommonModule],
+  imports: [
+    RouterOutlet,
+    NavbarComponent,
+    ProductListComponent,
+    CommonModule,
+    CategoryListComponent,
+  ],
   templateUrl: './app.component.html',
   styleUrl: './app.component.css',
 })
