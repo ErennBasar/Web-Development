9661cedee4962f1c33f6b689eaf5e5093d871e0e
Eren Basar <erenbasar16@hotmail.com>
2024-07-30 14:02:08 +0300

    card component has been created

---
+++ react-app/my-react-app/src/Card.jsx
@@ -0,0 +1,16 @@
+function Card() {
+  return (
+    <div class="card">
+      <img
+        class="card-image"
+        src="https://via.placeholder.com/150"
+        alt="profile picture"
+      />
+      <h2 class="card-title">Eren Basar</h2>
+      <p class="card-text">CS Student</p>
+      <hr />
+    </div>
+  );
+}
+
+export default Card;
