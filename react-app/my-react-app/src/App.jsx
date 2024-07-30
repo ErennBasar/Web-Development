d814d081d25a1220aec04c88d6328ff4c9704bb4
Eren Basar <erenbasar16@hotmail.com>
2024-07-30 20:25:14 +0300

    updated

--- react-app/my-react-app/src/App.jsx
+++ react-app/my-react-app/src/App.jsx
@@ -2,16 +2,22 @@ import Header from "./Header.jsx";
 import Footer from "./Footer.jsx";
 import Card from "./Card.jsx";
 import Button from "./button/Button.jsx";
+import Student from "./Student.jsx";
 
 function App() {
   return (
     <>
-      <Button></Button>
       <Header></Header>
+      <Button></Button>
+      <Student name="Ahmet" age={30} isStudent={false}></Student>
+      <Student name="Eren" age={23} isStudent={true}></Student>
+      <Student name="Ali" age={38} isStudent={false}></Student>
+      <Student age={21}></Student>
+      <Card name="Eren" job="CS Student"></Card>
+      <Card name="Ahmet" job="DEV Ops"></Card>
+      <Card name="Ahmet" job="Project Manager"></Card>
       <Card></Card>
-      <Card></Card>
-      <Card></Card>
-      <Card></Card>
+
       <Footer></Footer>
     </>
   );
