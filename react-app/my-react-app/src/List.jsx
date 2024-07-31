c153c7ea3e85d0b9f29a9387a8eaf5c0db3e57b6
Eren Basar <erenbasar16@hotmail.com>
2024-07-31 22:06:26 +0300

    css update

--- react-app/my-react-app/src/List.jsx
+++ react-app/my-react-app/src/List.jsx
@@ -22,8 +22,8 @@ function List({category="Category",items=[]}){
 
     return(
             <>
-             <h3> {category} </h3>
-             <ol>{listItems}</ol>
+             <h3 className="list-category"> {category} </h3>
+             <ol className="list-items">{listItems}</ol>
             </>
         );
 
