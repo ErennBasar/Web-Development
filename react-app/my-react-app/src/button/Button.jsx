6cc07f3c88d8c563838a344b0602053352924bfe
Eren Basar <erenbasar16@hotmail.com>
2024-08-01 03:37:11 +0300

    button click function has been added

--- react-app/my-react-app/src/button/Button.jsx
+++ react-app/my-react-app/src/button/Button.jsx
@@ -1,7 +1,26 @@
 import styles from "./Button.module.css";
 
 function Button() {
-  return <button className={styles.button}>Click</button>;
+  /*
+  const clickEvent = () => console.log("clicked");
+
+  const clickEvent2 = (name) => console.log(`${name}stop clicking`);
+
+  return <button className={styles.button} onClick={()=> clickEvent2("eren ")} >Click </button>;
+  */
+
+  let count = 0;
+  
+  const handleClick = (name) => {
+
+       count++;
+    
+       count < 3 ? console.log(`${name} clicked ${count} times`) : console.log(`${name} stop clicking me`) ;
+    };
+    
+
+  return <button className={styles.button} onClick={()=>handleClick("bro")} > Click </button>;
+
 }
 
 export default Button;
