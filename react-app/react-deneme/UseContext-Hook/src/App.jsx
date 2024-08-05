4aa987002b2258f3c0962b6b476f3f92100397ab
Eren Basar <erenbasar16@hotmail.com>
2024-08-06 00:44:39 +0300

    useContext Hook example

---
+++ react-app/react-deneme/UseContext-Hook/src/App.jsx
@@ -0,0 +1,14 @@
+import { useState } from 'react'
+import './App.css'
+import ComponentA from './ComponentA'
+
+function App() {
+  
+  return(
+    <ComponentA></ComponentA>
+  )
+
+ 
+}
+
+export default App
